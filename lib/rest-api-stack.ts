378db2c17f102e55a58b906c46c7f910dc72abe3
kelite12 <20108870@mail.wit.ie>
2025-03-09 21:57:01 +0000

    Exercise: Delete movie endpoint

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -48,10 +48,23 @@ export class RestApiStack extends cdk.Stack {
       },
     });
 
+    const deleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambdas/deleteMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
     // Permissions
     moviesTable.grantReadData(getAllMoviesFn);
     moviesTable.grantReadData(getMovieByIdFn);
     moviesTable.grantReadWriteData(addMovieFn);
+    moviesTable.grantReadWriteData(deleteMovieFn);
 
     // REST API
     const api = new apig.RestApi(this, "RestAPI", {
@@ -83,6 +96,10 @@ export class RestApiStack extends cdk.Stack {
     specificMovieEndpoint.addMethod(
       "GET",
       new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
+    );
+    specificMovieEndpoint.addMethod(
+      "DELETE",
+      new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
     );
   }
 }
